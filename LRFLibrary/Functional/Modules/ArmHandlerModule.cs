233f4f4297f8fbcd13175ddf6de4a9277270d540
Alejandro Cena <alejandro.cena@gmail.com>
2014-06-14 10:27:17 -0300

    Creacion del manejador de Brazo y el selector de eskeletos.

---
+++ LRFLibrary/Functional/Modules/ArmHandlerModule.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+using Microsoft.Xna.Framework;
+
+namespace LRFLibrary.Functional.Modules
+{
+    using Arm;
+    using LRFLibrary.Functional.Skeletal.Tracking;
+
+    public class ArmHandlerModule : IModule
+    {
+        private SkeletalSelector _selector;
+        private IArm _arm;
+
+        public ArmHandlerModule(SkeletalSelector selector, IArm arm)
+        {
+            this._arm = arm;
+            this._selector = selector;
+        }
+        
+        public static float AngleBetweenTwoVectors(Vector3 vectorA, Vector3 vectorB)
+        {
+            float dotProduct = 0.0f;
+            dotProduct=  Vector3.Dot( vectorA,  vectorB );
+            return (float)Math.Acos(dotProduct); 
+        }
+
+        void _selector_onSkeletonUpdated(object sender, SkeletalSelector.SkeletonArgs e)
+        {
+            Vector3 hand = new Vector3(
+                e.Skeleton.Joints[JointType.HandRight].Position.X,
+                e.Skeleton.Joints[JointType.HandRight].Position.Y,
+                e.Skeleton.Joints[JointType.HandRight].Position.Z
+            );
+
+            Vector3 elbow = new Vector3(
+                e.Skeleton.Joints[JointType.ElbowRight].Position.X,
+                e.Skeleton.Joints[JointType.ElbowRight].Position.Y,
+                e.Skeleton.Joints[JointType.ElbowRight].Position.Z
+            );
+
+            Vector3 shoulder = new Vector3(
+                e.Skeleton.Joints[JointType.ShoulderRight].Position.X,
+                e.Skeleton.Joints[JointType.ShoulderRight].Position.Y,
+                e.Skeleton.Joints[JointType.ShoulderRight].Position.Z
+            );
+
+            Vector3 wrist = new Vector3(
+                e.Skeleton.Joints[JointType.WristRight].Position.X,
+                e.Skeleton.Joints[JointType.WristRight].Position.Y,
+                e.Skeleton.Joints[JointType.WristRight].Position.Z
+            );
+
+            Vector3 a1 = hand - elbow;
+            Vector3 a2 = elbow - elbow;
+            Vector3 a3 = elbow - shoulder;
+            //Vector3 a4 = elbow - shoulder;
+
+            //Vector3 a4 = a1 - a2;
+            /*
+            this._arm.moveElbowTo(AngleBetweenTwoVectors(b1, b2));
+            this._arm.moveElbowTo(AngleBetweenTwoVectors(b1, b2));
+            this._arm.moveElbowTo(AngleBetweenTwoVectors(b1, b2));
+            this._arm.moveElbowTo(AngleBetweenTwoVectors(b1, b2));
+            */
+        }
+
+        public void run()
+        {
+            this._selector.Enable();
+            this._selector.onSkeletonUpdated += _selector_onSkeletonUpdated;
+        }
+
+        public void stop()
+        {
+            this._selector.onSkeletonUpdated -= _selector_onSkeletonUpdated;
+            this._selector.Disable();
+        }
+    }
+}
---
+++ LRFLibrary/Functional/Skeletal/Tracking/SkeletalSelector.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.Kinect;
+
+namespace LRFLibrary.Functional.Skeletal.Tracking
+{
+    using Skeletal.Condition;
+    using Skeletal.Tracking;
+    using Navigator;
+
+    public class SkeletalSelector
+    {
+        private INavigator navigator;
+        private SkeletalTracker tracker;
+        private ISkeltalCondition acceptance;
+        private ISkeltalCondition decline;
+        public Skeleton[] Skeltons { get; private set; }
+        protected Skeleton trackedSkeleton;
+
+        public event EventHandler<SkeletonArgs> OnSkeletonTracked;
+        public event EventHandler<EventArgs> onSkeletonNotTracked;
+        public event EventHandler<SkeletonArgs> onSkeletonUpdated;
+
+        public SkeletalSelector(SkeletalTracker tracker)
+        {
+            this.tracker = tracker;
+            this.acceptance = new OneHandUp();
+            this.decline = new TwoHandsUp();
+        }
+
+        public SkeletalSelector(SkeletalTracker tracker, ISkeltalCondition acceptance, ISkeltalCondition decline)
+        {
+            this.tracker = tracker;
+            this.acceptance = acceptance;
+            this.decline = decline;
+        }
+
+        private void PeopleFollower_SkeletonFrameReady(object sender, Skeleton[] skeletons)
+        {
+            this.Skeltons = skeletons;
+            updateTrackedSkeleton();
+            checkTrackingAcceptanceOrDecline();
+        }
+
+        private void updateTrackedSkeleton()
+        {
+            if (trackedSkeleton != null)
+            {
+                foreach (Skeleton skel in Skeltons)
+                {
+                    if (skel.TrackingState == SkeletonTrackingState.Tracked && trackedSkeleton.TrackingId.Equals(skel.TrackingId))
+                    {
+                        trackedSkeleton = skel;
+                        if (this.onSkeletonUpdated != null)
+                        {
+                            this.onSkeletonUpdated(this, new SkeletonArgs(trackedSkeleton));
+                        }
+                    }
+                }
+            }
+        }
+
+        private void checkTrackingAcceptanceOrDecline()
+        {
+            bool someoneWasTracked = false;
+            foreach (Skeleton skel in Skeltons)
+            {
+                if (skel.TrackingState == SkeletonTrackingState.Tracked)
+                {
+                    someoneWasTracked = true;
+                    if (acceptance.apply(skel))
+                    {
+                        this.tracker.Track(skel);
+                        trackedSkeleton = skel;
+                        if (this.OnSkeletonTracked != null)
+                        {
+                            this.OnSkeletonTracked(this, new SkeletonArgs(trackedSkeleton));
+                        }
+                    }
+                    if (decline.apply(skel))
+                    {
+                        this.tracker.NoTrack();
+                        trackedSkeleton = null;
+
+                        if (this.onSkeletonNotTracked != null)
+                        {
+                            this.onSkeletonNotTracked(this, new EventArgs());
+                        }
+                    }
+                }
+            }
+            if (!someoneWasTracked)
+            {
+                tracker.NoTrack();
+                trackedSkeleton = null;
+                if (this.onSkeletonNotTracked != null)
+                {
+                    this.onSkeletonNotTracked(this, new EventArgs());
+                }
+            }
+        }
+
+        public void Enable()
+        {
+            tracker.SkeletonFrameReady += PeopleFollower_SkeletonFrameReady;
+            tracker.NoTrack();
+            tracker.Enable();
+        }
+
+        public void Disable()
+        {
+            tracker.SkeletonFrameReady -= PeopleFollower_SkeletonFrameReady;
+            tracker.NoTrack();
+            tracker.Disable();
+        }
+
+        public class SkeletonArgs : EventArgs
+        {
+            Skeleton skel;
+            public Skeleton Skeleton
+            {
+                get
+                {
+                    return skel;
+                }
+            }
+
+            public SkeletonArgs(Skeleton skel)
+            {
+                this.skel = skel;
+            }
+        }
+    }
+}
